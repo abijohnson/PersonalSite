88869d6a2483047ccc46cb56c2b5909db551af8f
abijohnson <aljohnson18@mail.wou.edu>
2020-07-21 17:09:33 -0700

    Adding all files!

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("navbar").innerHTML =
+  '<a href="index.html" id="nav-title">Abi Johnson</a>' +
+  '<ul id="navbar-nav">' +
+  '<li><a href="contact.html">Contact</a></li>' +
+  '<li><a href="about.html">About</a></li>' +
+  '<li><a href="resume.html">Resume</a></li>' +
+  "</ul>";
