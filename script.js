f268b00c90ba271004a45c7c0acd0a0d661447de
abijohnson <aljohnson18@mail.wou.edu>
2020-08-06 18:48:21 -0700

    More mobile improvements, specifically to resume

--- script.js
+++ script.js
@@ -18,8 +18,98 @@ $(document).ready(function () {
         '<li><a href="about.html">About</a></li>' +
         '<li><a href="resume.html">Resume</a></li>' +
         "</ul>";
+
+      var x = document.getElementById("resume-lang-list");
+      x.style.display = "block";
+
+      var y = document.getElementById("resume-tools-list");
+      y.style.display = "block";
+
+      var z = document.getElementById("resume-projects");
+      z.style.display = "block";
+
+      var a = document.getElementById("resume-experience");
+      a.style.display = "block";
+
+      var b = document.getElementById("resume-education");
+      b.style.display = "block";
     }
   }
   checkWidth();
   $(window).resize(checkWidth);
 });
+
+function displayLangList() {
+  var windowSize = $(window).width();
+  if (windowSize <= 700) {
+    var x = document.getElementById("resume-lang-list");
+    if (x.style.display == "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } else {
+    var x = document.getElementById("resume-lang-list");
+    x.style.display = "block";
+  }
+}
+
+function displayToolsList() {
+  var windowSize = $(window).width();
+  if (windowSize <= 700) {
+    var x = document.getElementById("resume-tools-list");
+    if (x.style.display == "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } else {
+    var x = document.getElementById("resume-tools-list");
+    x.style.display = "block";
+  }
+}
+
+function displayProjects() {
+  var windowSize = $(window).width();
+  if (windowSize <= 700) {
+    var x = document.getElementById("resume-projects");
+    if (x.style.display == "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } else {
+    var x = document.getElementById("resume-projects");
+    x.style.display = "block";
+  }
+}
+
+function displayExperience() {
+  var windowSize = $(window).width();
+  if (windowSize <= 700) {
+    var x = document.getElementById("resume-experience");
+    if (x.style.display == "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } else {
+    var x = document.getElementById("resume-experience");
+    x.style.display = "block";
+  }
+}
+
+function displayEducation() {
+  var windowSize = $(window).width();
+  if (windowSize <= 700) {
+    var x = document.getElementById("resume-education");
+    if (x.style.display == "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } else {
+    var x = document.getElementById("resume-education");
+    x.style.display = "block";
+  }
+}
